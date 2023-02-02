a6a7231f57742d90636f307dc710391426cd731d
Vitaliy <vitaliyfiniuk@gmail.com>
2023-02-03 00:07:33 +0200

    First Commit
    First Commit

---
+++ Unity-AnimationUI-main/Assets/AnimationUI/Script/Other/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource _soundSource;
+    // [SerializeField] AudioMixer _soundMixer;
+    [SerializeField] AudioClip _defaultSound;
+
+    public void PlaySound(AudioClip audioClip, float volume)
+    {
+        if(audioClip == null)
+        {
+            PlayDefaultSound();
+            return;
+        }
+        _soundSource.PlayOneShot(audioClip, volume);
+    }
+    public void PlaySound(AudioClip audioClip)
+    {
+        if(audioClip == null)
+        {
+            PlayDefaultSound();
+            return;
+        }
+        _soundSource.PlayOneShot(audioClip);
+    }
+    void PlayDefaultSound()
+    {
+        if(_defaultSound != null)
+        _soundSource.PlayOneShot(_defaultSound);
+    }
+
+    [System.Serializable] public class Sound
+    {
+        [Tooltip("Clip to play")]public AudioClip Clip;
+        [Tooltip("Volume of the clip")]
+        public float Volume = 1;
+        #if UNITY_EDITOR 
+        [Tooltip("Just for naming, this isn't actually used anywhere")]public string ClipName;
+        #endif
+    }
+    public Sound[] SFX;
+    public void PlaySound(int index)
+    {
+        if(index > SFX.Length-1)
+        {
+            Debug.LogWarning("Please assign the clip at index " + index.ToString());
+        }
+        PlaySound(SFX[index].Clip, SFX[index].Volume);
+    }
+}
---
+++ Unity-AnimationUI-main/Assets/AnimationUI/Script/Other/Singleton.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class Singleton : MonoBehaviour
+{
+    public AudioManager Audio;
+    public GameManager Game;
+    public static Singleton Instance
+    {
+        get
+        {
+            if(_instance == null)LoadSingleton();
+            return _instance;
+        }
+    }
+    static Singleton _instance;
+
+    
+    void Awake()
+    {
+        if(_instance == null)_instance = this;
+
+#if UNITY_EDITOR
+        else if(!Application.isPlaying)DestroyImmediate(gameObject);
+#endif
+        else Destroy(gameObject);
+
+#if UNITY_EDITOR
+        if(Application.isPlaying)
+#endif
+        DontDestroyOnLoad(gameObject);
+
+    }
+    
+    public static void LoadSingleton()
+    {
+        GameObject singleton = Resources.Load("SINGLETON") as GameObject;
+        if(singleton == null)
+        {
+            Debug.Log("SINGLETON prefab not found in .../Resources/SINGLETON. Please don't remove or move this to other folder.", singleton);
+            return;
+        }
+
+#if UNITY_EDITOR
+        UnityEditor.PrefabUtility.InstantiatePrefab(singleton);
+#else
+        Instantiate(singleton);
+#endif
+
+        if(_instance == null)
+        {
+            Debug.Log("Something went wrong with loading singleton", Singleton._instance);
+            return;
+        }
+        Debug.Log("Automatically loaded Singleton from .../Resources/SINGLETON");
+    }
+}
